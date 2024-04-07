3b53a8a613e028aaafb88c2228756c078b8cbe36
hepitk <heino.pitkanen@helsinki.fi>
2024-04-07 23:09:40 +0300

    fixed all

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require('express');
+const Person = require('./models/persons');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-const Person = require('./models/persons');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-morgan.token('body', (req) => {
-    return JSON.stringify(req.body);
-});
+morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-app.use((error, req, res, next) => {
-    console.error(error.message);
-
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message });
-    } else if (error.name === 'MongoServerError' && error.code === 11000) {
-        return res.status(400).json({ error: 'name must be unique' });
-    }
-
-    next(error);
-});
-
-const persons = [
-    { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
-];
 
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
@@ -41,12 +18,10 @@ app.get('/api/persons', (req, res) => {
     });
 });
 
-app.get('/info', (req, res, next) => {
-    Person.countDocuments({})
-        .then(count => {
-            res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`);
-        })
-        .catch(error => next(error));
+app.get('/info', (req, res) => {
+    Person.countDocuments({}).then(count => {
+        res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`);
+    });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -55,36 +30,48 @@ app.get('/api/persons/:id', (req, res, next) => {
             if (person) {
                 res.json(person);
             } else {
-                res.status(404).end();
+                res.status(404).send({ error: 'Entry not found' });
             }
         })
         .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            if (result) {
-                res.status(204).end();
-            } else {
-                res.status(404).json({ error: 'Entry not found' });
-            }
-        })
+app.post('/api/persons', (req, res, next) => {
+    const { name, number } = req.body;
+    const person = new Person({ name, number });
+
+    person.save()
+        .then(savedPerson => res.json(savedPerson))
         .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
     const { name, number } = req.body;
 
-    Person.findByIdAndUpdate(
-        req.params.id,
-        { name, number },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedPerson => {
-            res.json(updatedPerson);
+    Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error));
+});
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            if (result) res.status(204).end();
+            else res.status(404).send({ error: 'Entry not found' });
         })
         .catch(error => next(error));
+});
+
+app.use((error, req, res, next) => {
+    console.error(error.message);
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message });
+    } else if (error.name === 'MongoServerError' && error.code === 11000) {
+        return res.status(400).json({ error: 'name must be unique' });
+    }
+    next(error);
 });
 
 const PORT = process.env.PORT || 3001;
